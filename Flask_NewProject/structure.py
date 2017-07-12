f1094058a724066dab3eae18fe6684a473a35669
= <GunaksGit@gmail.com>
2017-07-12 09:24:37 -0700

    workng on building the base class

--- Flask_NewProject/structure.py
+++ Flask_NewProject/structure.py
@@ -2,26 +2,7 @@ from CreateDirs import create_dirs
 
 
 class Structure:
-    """Creation of Flask directories."""
+    """Base class of structuring a Flask Application."""
 
-    def __init__(self, projectName):
-        self.projectName = projectName
-
-    def simple_setup(self, preload=False):
-        pass
-
-
-    def simple_package(self, preload=False):
-        pass
-
-    def blueprint_package(self, sytle="divisional", preload=False):
-        if style.lower() == "functional":
-            pass
-        
-        elif style.lower() == "divisional":
-            pass
-        
-        else:
-            print("Not a known style.")
-
+    pass
     
